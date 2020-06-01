bd942e5bad1b656a8707c5d21f24e4783976d82d
solh0820 <solh0820@gmail.com>
2020-05-31 23:44:32 -0500

    Send Assigment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## this function will create a matrix-object that can be used in order to calculate the inverse
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+makeCacheMatrix <- function(x = matrix()) {## We start defining the parameters of the function setting it as matrix
+  inver <- NULL                             ## initialize matrix
+  set <- function(y) {                      ## this function help us defining the set function to assign 
+    x <<- y                            
+    inver <<- NULL                          ## reinit, the matrix in case it exits a new one
+  }
+  getv <- function() x                     ##  get the value of the matrix 
+  
+  setinver <- function(inversa) inver <<- inversa  ## assigns value inver in upper enviroments
+  valinver <- function() inver                     ## gets the value of inver 
+  list(set = set, getv = getv, setinver = setinver, valinver = valinver)  ## we stablish this list in order to link functions with the $ operator
+    }
 
 
 ## Write a short comment describing this function
+## This function calculates the inverse of the matrix in the above function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inver <- x$valinver()
+  if(!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data, ...)
+  x$setinver(inver)
+  inver
+       
 }
